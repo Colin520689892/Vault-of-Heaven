f52a6f83aebc8106e7627245838df7fea84162f3
Colin520689892 <1485940063@qq.com>
2024-05-15 21:53:53 +0800

    公共字段自动填充

--- sky-common/src/main/java/com/sky/context/BaseContext.java
+++ sky-common/src/main/java/com/sky/context/BaseContext.java
@@ -1,7 +1,8 @@
 package com.sky.context;
 
 public class BaseContext {
-
+    //每一个上下文创建了一个线程变量,用来存储long类型的id
+    //创建三个方法,用来设置,取用,删除id
     public static ThreadLocal<Long> threadLocal = new ThreadLocal<>();
 
     public static void setCurrentId(Long id) {
